9d4799f3dc2be3d8038a0ce88bc9d5077f6e7226
Max <90206985+Elsyvien@users.noreply.github.com>
2025-08-05 15:25:31 +0200

    Update MainWindow.h

--- ui/MainWindow.h
+++ ui/MainWindow.h
@@ -3,9 +3,23 @@
 #include <QStandardItemModel>
 #include "PythonBridge.h"
 
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
+    ~MainWindow();
+
+private slots:
+    void onData(const QJsonObject& payload);
+    void onFailed(const QString& msg);
+    void fetchNow();
+
+private:
+    Ui::MainWindow *ui;
+    QStandardItemModel* model;
+    PythonBridge* bridge;
 };
